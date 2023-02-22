884d815705db7f61164b452fa6eb0f2d0a944f73
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-02-22 23:25:05 +0530

    feat : authentication setup

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`connected to mongodb database ${conn.connection.host}`.bgGreen.white)
+    } catch (error) {
+        console.log(`Error in MongoDB ${error}`.bgRed.white)
+    }
+}
+
+export default connectDB;
---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import colors from 'colors';
+import  dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js'
+import authRoutes from './routes/authRoute.js'
+
+// configure env
+dotenv.config();
+
+// database config
+connectDB();
+
+// Rest object
+const app = express();
+
+//middlewares
+app.use(express.json())
+app.use(morgan("dev"))
+
+// routes
+app.use("/api/v1/auth", authRoutes)
+
+// Rest Api's
+app.get('/', (req, res) => {
+    res.send(
+     "<h1>welcome to the snitch Shoping</h1>"
+    )
+})
+
+// PORT 
+const PORT = process.env.PORT || 8080;
+
+// RUN listen
+app.listen(PORT, () => {
+    console.log(`Server listening on ${process.env.DEV_MODE} on ${PORT}`.bgCyan.white);
+})
