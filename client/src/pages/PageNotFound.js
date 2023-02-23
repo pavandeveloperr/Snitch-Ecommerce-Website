92e9c43a82c75434a9b37f8c80733b3125f1f658
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-02-24 01:26:25 +0530

    feat : 404 page styling

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Layout from "../Components/Layout/Layout";
 
 const PageNotFound = () => {
   return (
-    <Layout>
-      <h1>Page Not Found</h1>
-    </Layout>
+  <Layout>
+    <div className="pnf">
+      <h1 className="pnf-title">
+        404
+      </h1>
+      <h2 className="pnf-heading">
+        Oops ! Page Not Found
+      </h2>
+      <Link to='/' className="pnf-btn">
+        Go Back
+      </Link>
+    </div>
+  </Layout>
   );
 };
 
