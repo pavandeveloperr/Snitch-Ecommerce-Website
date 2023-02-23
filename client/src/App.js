1526bf5f656c3aeef43b9adba3066233cadceff3
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-02-24 01:27:00 +0530

    style : footer styling ✅

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Route path='/' element={<Homepage />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/contact' element={<Contact />}/>
-        <Route path='/policy' element={<Policy />}/>
+        <Route path='/privacypolicy' element={<Policy />}/>
         <Route path='*' element={<PageNotFound />}/>
       </Routes>
     </>
--- client/src/Components/Layout/Footer.js
+++ client/src/Components/Layout/Footer.js
@@ -1,13 +1,79 @@
-import React from 'react'
-
+import React from "react";
+import {SlSocialFacebook, SlSocialInstagram, SlSocialLinkedin, SlSocialTwitter} from 'react-icons/sl';
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className='bg-dark text-light p-3'>
-        <h1 className='text-center'>
-            All Rights Reserved &copy; Pavan Kulkarni
-        </h1>
-        </div>
-  )
-}
+    <div
+      className="container-fluid text-white p-3"
+      style={{
+        fontFamily: "Poppins, sans-serif",
+        background: "linear-gradient(to right, #24243e, #302b63, #0f0c29)",
+      }}
+    >
+      <div className="mt-3">
+        <h3 className="text-center">Want to Partner with us ?</h3>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <center><button className="btn btn-block btn-outline-light">Get Started</button></center>
+        <hr />
+      </div>
 
-export default Footer
+      <div className="row justify-content-around text-center text-md-start">
+        <div className="col-md-2 text-center">
+          <h4 className="footer-brand">SNITCH</h4>
+          <p>Discover Your Style</p>
+        </div>
+        <div className="col-md-2">
+          <ul className="list-unstyled">
+            <li className="fw-bold my-2">Links</li>
+            <li><Link to='/privacypolicy' className="text-decoration-none text-white">Privacy Policy</Link></li>
+            <li><a href='#' className="text-decoration-none text-white">Social Media</a></li>
+            <li><a href='#' className="text-decoration-none text-white">Branding</a></li>
+          </ul>
+        </div>
+
+        <div className="col-md-2">
+          <ul className="list-unstyled">
+            <li className="fw-bold my-2">About</li>
+            <li><Link to='/about' className="text-decoration-none text-white">Our Project</Link></li>
+            <li><a href="#" className="text-decoration-none text-white">Careers</a></li>
+          </ul>
+        </div>
+
+        <div className="col-md-2">
+          <ul className="list-unstyled">
+            <li className="fw-bold my-2">Support</li>
+            <li><Link to='/contact' className="text-decoration-none text-white">Contact</Link></li>
+            <li><a href="#" className="text-decoration-none text-white">Support Request</a></li>
+          </ul>
+        </div>
+
+        <div className="col-md-2">
+            <li className="fw-bold my-2 list-unstyled mx-5">Follow us</li>
+            <ul className="list-unstyled d-flex justify-content-center justify-conten-md-start">
+            <li><a href="#" className="text-decoration-none text-white mx-2"><SlSocialInstagram /></a></li>
+            <li><a href="#" className="text-decoration-none text-white mx-2"><SlSocialLinkedin /></a></li>
+            <li><a href="#" className="text-decoration-none text-white mx-2"><SlSocialFacebook /></a></li>
+            <li><a href="#" className="text-decoration-none text-white mx-2"><SlSocialTwitter /></a></li>
+          </ul>
+        </div>
+
+        <hr />
+
+        <div className="row">
+          <div className="col-md-12 text-center pt-2">
+            <p>&copy; 2023 Copyright <a href="https://linkedin.com/in/pavankulkarnii" target="_blank" className="text-decoration-none text-white">Pavan Kulkarni</a></p>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
