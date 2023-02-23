f539d1d3c80b2e5e5e8b9786cd166f8b076fb825
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-02-23 18:55:09 +0530

    feat : App layout & routing setup ✅

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import {Routes, Route} from 'react-router-dom'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
+import Policy from './pages/Policy';
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/policy' element={<Policy />}/>
+        <Route path='*' element={<PageNotFound />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-3'>
+        <h1 className='text-center'>
+            All Rights Reserved &copy; Pavan Kulkarni
+        </h1>
+        </div>
+  )
+}
+
+export default Footer
