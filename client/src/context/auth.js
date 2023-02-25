793c988bee2a08b913dac473d3ea23814e3caeb5
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-02-25 14:00:59 +0530

    fix : useEff dependency arr remove [BUG] 🐞

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,12 +1,25 @@
-import { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
+  // GLOBAL STATES
   const [auth, setAuth] = useState({
     user: null,
     token: "",
-  })
+  });
+  useEffect(() => {
+    const data = localStorage.getItem('auth')
+    if(data) {
+      const parseData = JSON.parse(data);
+      setAuth({
+        ...auth,
+        user: parseData.user,
+        token: parseData.token
+      })
+    }
+    //eslint-disable-next-line
+  }, [])
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
         {children}
