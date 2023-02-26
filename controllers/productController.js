7b4233d5fb2349399e88934c9d91bc4474c23d26
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-02-27 01:11:04 +0530

    feat : backend paginate homepage products✅

--- controllers/productController.js
+++ controllers/productController.js
@@ -177,3 +177,70 @@ export const updateProductController = async (req, res) => {
     });
   }
 };
+
+// filter controller
+export const productFiltersController = async (req, res) => {
+  try {
+    const { checked, radio } = req.body;
+    let args = {};
+    if (checked.length > 0) args.category = checked;
+    if (radio.length) args.price = { $gte: radio[0], $lte: radio[1] };
+    const products = await productModel.find(args);
+    res.status(200).send({
+      success: true,
+      message: "Filters applied successfully",
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      error,
+      message: "Error While Filtering Products",
+    });
+  }
+};
+
+// product count controller
+export const productCountController = async (req, res) => {
+  try {
+    const total = await productModel.find({}).estimatedDocumentCount();
+    res.status(200).send({
+      success: true,
+      total,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      error,
+      message: "Error in product count",
+    });
+  }
+};
+
+// product list base on page
+
+export const productListController = async (req, res) => {
+  try {
+    const perPage = 3;
+    const page = req.params.page ? req.params.page : 1;
+    const products = await productModel
+      .find({})
+      .select("-image")
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .sort({ createdAt: -1 });
+      res.status(200).send({
+        success: true,
+        products
+      })
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      error,
+      message: "Error in per page ctrl",
+    });
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,17 @@
 import express from "express";
-import { createProductController, deleteProductController, getProductController, getProductImageController, getSingleProductController, updateProductController } from "../controllers/productController.js";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getProductImageController,
+  getSingleProductController,
+  productCountController,
+  productFiltersController,
+  productListController,
+  updateProductController,
+} from "../controllers/productController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import formidable from 'express-formidable'
+import formidable from "express-formidable";
 
 const router = express.Router();
 
@@ -16,26 +26,34 @@ router.post(
   createProductController
 );
 
-// UPDATE PRODUCT 
+// UPDATE PRODUCT
 router.put(
-    "/update-product/:pid",
-    requireSignIn,
-    isAdmin,
-    formidable(),
-    updateProductController
-  );
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
 
 // GET PRODUCTS || GET
-router.get('/get-product', getProductController);
+router.get("/get-product", getProductController);
 
 // GET SINGLE PRODUCT || GET
-router.get('/get-product/:slug', getSingleProductController);
+router.get("/get-product/:slug", getSingleProductController);
 
 // GET PRODUCT IMAGE || GET
-router.get('/product-image/:pid', getProductImageController);
-
+router.get("/product-image/:pid", getProductImageController);
 
 // DELETE PRODUCT  || DELETE
-router.delete('/delete-product/:pid', deleteProductController);
+router.delete("/delete-product/:pid", deleteProductController);
+
+// FILTER PRODUCT || POST
+router.post("/product-filters", productFiltersController);
+
+// PRODUCT COUNT || GET
+router.get("/product-count", productCountController);
+
+// PRODUCT PER PAGE || GET
+router.get("/product-list/:page", productListController);
 
 export default router;
