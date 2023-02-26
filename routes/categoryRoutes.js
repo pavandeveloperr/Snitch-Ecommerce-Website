551c00c9ecfbf1d191c5eb2d1d5cca11e0d785d8
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-02-26 15:01:15 +0530

    feat : category backend api ✅

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from "../controllers/CreateCategoryController.js";
+import { requireSignIn, isAdmin } from '../middlewares/authMiddleware.js'
+
+const router = express.Router();
+
+// routes
+
+// create category
+router.post('/create-category', requireSignIn, isAdmin, createCategoryController);
+
+// update category
+router.put("/update-category/:id", requireSignIn, isAdmin, updateCategoryController)
+
+// Get All category
+router.get("/all-category", categoryController)
+
+// Get single category
+router.get("/single-category/:slug", singleCategoryController)  
+
+// Delete category
+router.delete("/delete-category/:id", requireSignIn, isAdmin, deleteCategoryController)  
+
+
+export default router;
